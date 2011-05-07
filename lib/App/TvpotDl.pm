36606c7778f7f1c054ff05f9cf94e11982723f43
Seungwon Jeong <seungwon0@gmail.com>
2011-05-07 12:13:47 +0900

    Updated Changes

--- lib/App/TvpotDl.pm
+++ lib/App/TvpotDl.pm
@@ -18,11 +18,11 @@ App::TvpotDl - Download flash videos from Daum tvpot
 
 =head1 VERSION
 
-Version 0.9.0
+Version 0.9.1
 
 =cut
 
-our $VERSION = '0.9.0';
+our $VERSION = '0.9.1';
 
 =head1 SYNOPSIS
 
